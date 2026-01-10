4e18a639b4ed94bdf7890b0c3e9b29f0a3849751
ratul <ratulbmg@gmail.com>
2026-01-11 02:16:02 +0530

    fixed dashboard  darkmode

--- apps/web/src/pages/Dashboard/Dashboard.tsx
+++ apps/web/src/pages/Dashboard/Dashboard.tsx
@@ -8,15 +8,15 @@ const Dashboard: React.FC = () => {
     const [createContentIsOpen, setCreateContentIsOpen] = useState(false);
     return (
         <>
-            <div className={cn('m-auto max-w-[1536px] w-full min-h-screen h-full flex justify-center items-center')}>
+            <div className={cn('m-auto bg-white dark:bg-black/85 w-full min-h-screen h-full flex justify-center items-center')}>
                 <div className={cn('w-auto h-auto flex flex-col-reverse sm:flex-row justify-center items-center gap-8 sm:gap-4')}>
                     <div className={cn('flex justify-center items-center')}>
-                        <p className={cn('text-[30px] bg-[#898989] text-white flex justify-center items-center rounded-full p-2')}> <IoIosArrowDroprightCircle /></p>
-                        <h1 className={cn('text-[30px] text-[#898989] rounded-[5px] px-2')}>All Collection</h1>
+                        <p className={cn('text-[30px] text-gray-500 bg-white flex justify-center items-center rounded-full p-2')}> <IoIosArrowDroprightCircle /></p>
+                        <h1 className={cn('text-[30px] text-gray-500 rounded-[5px] px-2')}>All Collection</h1>
                     </div>
-                    <hr className={cn('text-[#898989] w-[250px] sm:w-[100px] sm:block sm:rotate-90')} />
+                    <hr className={cn('text-gray-500 w-[250px] sm:w-[100px] sm:block sm:rotate-90')} />
                     <div>
-                        <RiAddCircleLine className={cn(' text-[#898989] text-[5em]')} onClick={() => setCreateContentIsOpen(true)} />
+                        <RiAddCircleLine className={cn(' text-gray-500 text-[5em]')} onClick={() => setCreateContentIsOpen(true)} />
                     </div>
                     <ContentCreateModal
                         isOpen={createContentIsOpen}
